2aa8e07b008f53062f7c2629f22b56d94a70a1fa
Harshit-20 <hchoubey97@gmail.com>
2017-10-28 04:08:18 +0530

    add and drop course

--- FusionIIIT/applications/academic_procedures/urls.py
+++ FusionIIIT/applications/academic_procedures/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^main/', views.academic_procedures, name='procedures'),
     url(r'^register/', views.register, name='register'),
     url(r'^finalRegister/', views.final_register, name='final_register'),
-    url(r'addCourse/', views.add_course, name='addCourse'),
+    url(r'^addCourse/', views.register, name='addCourse'),
     url(r'^dropCourse/', views.drop_course, name='dropCourse'),
 ]
--- FusionIIIT/applications/academic_procedures/views.py
+++ FusionIIIT/applications/academic_procedures/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models import Max
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
 # from applications.academic_procedures.models import Register
@@ -24,16 +24,17 @@ def academic_procedures(request):
     current_user = get_object_or_404(User, username=request.user.username)
     user_details = ExtraInfo.objects.all().filter(user=current_user).first()
     student = Student.objects.all().filter(id=user_details.id).first()
-    # Fuction Call to get the current semster of the Student
+    # Function Call to get the current semester of the Student
     user_sem = get_user_semester(user_details.id)
 
     registered_or_not = Register.objects.all().filter(student_id=student).first()
-    # Fucntio call to get the current user's branch
+    # Function call to get the current user's branch
+    print(registered_or_not)
     user_branch = get_user_branch(user_details)
     user_sem = 2
 
     get_courses = Course.objects.all().filter(sem=(user_sem+1))
-    # Fucntion Call to get the courses related to the branch
+    # Function Call to get the courses related to the branch
     branch_courses = get_branch_courses(get_courses, user_branch)
     details = {
             'current_user': current_user,
@@ -42,7 +43,7 @@ def academic_procedures(request):
             }
     final_register = Register.objects.all().filter(student_id=user_details.id)
     final_register_count = FinalRegistrations.objects.all().filter(student_id=user_details.id)
-
+    add_course = get_add_course(branch_courses, final_register)
     print(final_register_count)
 
     return render(
@@ -50,12 +51,24 @@ def academic_procedures(request):
                                                                 'details': details,
                                                                 'courses_list': branch_courses,
                                                                 'final_register': final_register,
-                                                                'final_count': final_register_count
+                                                                'final_count': final_register_count,
+                                                                'add_course': add_course
                                                                 }
         )
 
 
+def get_add_course(branch, final):
+    x = []
+    for i in final:
+        x.append(i.course_id)
+    total_course = []
+    for i in branch:
+        if i not in x:
+            total_course.append(i)
+    return total_course
 # function to get user semester
+
+
 def get_user_semester(roll_no):
     roll = str(roll_no)
     roll = int(roll[:4])
@@ -76,7 +89,9 @@ def get_user_semester(roll_no):
 def get_branch_courses(courses, branch):
     course_list = []
     for course in courses:
-        if branch[:2] == course.course_id[:2].lower() or len(course.course_id) > 5:
+        if branch[:2].lower() == course.course_id[:2].lower() and len(course.course_id) == 5:
+            course_list.append(course)
+        if len(course.course_id) > 5:
             course_list.append(course)
     return course_list
 
@@ -86,60 +101,66 @@ def get_user_branch(user_details):
 
 
 @login_required(login_url='/accounts/login')
-def add_course(request):
-    if request.method=='POST':
-        print(request.POST)
-        return HttpResponse('Beauty')
-
-
 def drop_course(request):
-    current_user = get_object_or_404(User, username=request.user.username)
-    user_details = ExtraInfo.objects.all().filter(user=current_user).first()
-    # Fuction Call to get the current semster of the Student
-    user_sem = get_user_semester(user_details.id)
-    user_sem = 3
-    get_courses = Course.objects.all().filter(sem=user_sem)
-    print(get_courses, user_sem)
-    details = {'current_user': current_user, 'user_sem': user_sem}
-    return render(
-        request,
-        '../templates/academic_procedures/addCourse.html',
-        {'details': details, 'courses_list': get_courses})
+    # current_user = get_object_or_404(User, username=request.user.username)
+    if request.method == 'POST':
+        try:
+            c_id = request.POST.getlist('course_id')
+            user_id = request.POST.getlist('user')
+            for i in range(len(c_id)):
+                cour_id = Course.objects.all().filter(course_id=c_id[i]).first()
+                s_id = ExtraInfo.objects.all().filter(id=user_id[i][:7]).first()
+                s_id = Student.objects.all().filter(id=s_id)
+                instance = Register.objects.filter(course_id=cour_id).filter(student_id=s_id)
+                instance = instance[0]
+                print('ind=s', instance)
+                instance.delete()
+            messages.info(request, 'Successfully Dropped')
+            return HttpResponseRedirect('/academic-procedures/main')
+        except:
+            return HttpResponseRedirect('/academic-procedures/main')
+    else:
+        messages.info(request, 'Problem in dropping')
+        return HttpResponseRedirect('/academic-procedures/main')
 
 
 def register(request):
     if request.method == 'POST':
-        current_user = get_object_or_404(User, username=request.POST.get('user'))
-        current_user = ExtraInfo.objects.all().filter(user=current_user).first()
-        current_user = Student.objects.all().filter(id=current_user.id).first()
-        # current_user = get_object_or_404(ExtraInfo, user=current_user.username)
-        values_length = 0
-        for key, values in request.POST.lists():
-            values_length = len(values)
-            break
-        # Get the semester for the registration
-        sem = request.POST.get('semester')
-        for x in range(values_length):
-            for key, values in request.POST.lists():
-                if (key == 'choice'):
-                    last_id = Register.objects.all().aggregate(Max('r_id'))
-                    print(last_id)
-                    try:
-                        last_id = last_id['r_id__max']+1
-                    except:
-                        last_id = 1
-                    course_id = get_object_or_404(Course, course_id=values[x])
-                    p = Register(
-                        r_id=last_id,
-                        course_id=course_id,
-                        student_id=current_user,
-                        semester=sem
+        try:
+            current_user = get_object_or_404(User, username=request.POST.get('user'))
+            current_user = ExtraInfo.objects.all().filter(user=current_user).first()
+            current_user = Student.objects.all().filter(id=current_user.id).first()
+            # current_user = get_object_or_404(ExtraInfo, user=current_user.username)
+            values_length = 0
+            values_length = len(request.POST.getlist('choice'))
+            print(request.POST.getlist('choice'))
+            print(values_length)
+            # Get the semester for the registration
+            sem = request.POST.get('semester')
+            for x in range(values_length):
+                print(x)
+                for key, values in request.POST.lists():
+                    if key == 'choice':
+                        last_id = Register.objects.all().aggregate(Max('r_id'))
+                        print(last_id)
+                        try:
+                            last_id = last_id['r_id__max'] + 1
+                        except:
+                            last_id = 1
+                        course_id = get_object_or_404(Course, course_id=values[x])
+                        p = Register(
+                            r_id=last_id,
+                            course_id=course_id,
+                            student_id=current_user,
+                            semester=sem
                         )
-                    p.save()
-                else:
-                    continue
-        messages.info(request, 'Pre-Registration Successful')
-        return HttpResponseRedirect('/academic-procedures/main')
+                        p.save()
+                    else:
+                        continue
+            messages.info(request, 'Pre-Registration Successful')
+            return HttpResponseRedirect('/academic-procedures/main')
+        except:
+            return HttpResponseRedirect('/academic-procedures/main')
     else:
         print('not okay')
         return HttpResponseRedirect('/academic-procedures/main')
@@ -150,7 +171,7 @@ def final_register(request):
         current_user = get_object_or_404(User, username=request.user.username)
         extraInfo_user = ExtraInfo.objects.all().filter(user=current_user).first()
         current_user = Student.objects.all().filter(id=extraInfo_user.id).first()
-        # Fuction Call to get the current semster of the Student
+        # Function Call to get the current semester of the Student
         user_sem = get_user_semester(extraInfo_user.id)
         p = FinalRegistrations(
             reg_id=extraInfo_user,
@@ -161,8 +182,3 @@ def final_register(request):
         p.save()
         messages.info(request, 'Registration Successful')
     return HttpResponseRedirect('/academic-procedures/main')
-
-
-@login_required(login_url='/accounts/login')
-def drop_course(request):
-    pass
